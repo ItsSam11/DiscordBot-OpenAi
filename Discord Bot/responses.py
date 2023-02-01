4611a427e7975c12aba79b06e5c170abd35735ba
Marwan Samuel Flores Orozco <83468277+ItsSam11@users.noreply.github.com>
2023-02-01 15:05:18 -0600

    Update responses.py

--- Discord Bot/responses.py
+++ Discord Bot/responses.py
@@ -1,20 +1,35 @@
 import openai
-import random
 
 def mensaje_oai(message: str) -> str:
     oai_message = message.lower()
-
-    if message == 'roll':
-        return str(random.randint(1,10))
+  
+    openai.api_key = '****'
 
     if oai_message == '!help':
-        return '`Para hablar con el modelo de OpenAI, escribe de primero "-" al inicio de cada mensaje.`'
+        return '`Escribe "-Hola" para obtener más información.`'
+
+    if oai_message.startswith('-hola'):
+        return '''`Hola, soy Chatbot, un proyecto creado por Sam11 y WinstonTheBot, utilizo los recursos de OpenAI para ser más interactivo y estoy alojado en Amazon Web Services.`
+        \n`Si quieres usar las funciones de OpenAI tienes que iniciar cada mensaje con un "-" seguido de lo que quieres preguntar o decir.`
+        \n`Si quieres recibir mensajes privados de ChatBot tienes que iniciar con un "-?" seguido de lo que quieras preguntar o decir.`
+               '''
+
+    if message.lower().startswith('-?hola'):
+        return '''`Hola, soy Chatbot, un proyecto creado por Sam11 y WinstonTheBot, utilizo los recursos de OpenAI para ser más interactivo y estoy alojado en Amazon Web Services.`
+        \n`Si quieres usar las funciones de OpenAI tienes que iniciar cada mensaje con un "-" seguido de lo que quieres preguntar o decir.`
+        \n`Si quieres recibir mensajes privados de ChatBot tienes que iniciar con un "-?" seguido de lo que quieras preguntar o decir.`
+               '''
+    
+    if message.startswith('-?'):
+        text = message
+            
+        response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.7, max_tokens=4000)
+        
+        return response.choices[0].text
 
     if oai_message[0] == '-':
         text = oai_message
-        # Load your API key from an environment variable or secret management service
-        openai.api_key = '****'
-    
+        
         response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.7, max_tokens=4000)
         
         return response.choices[0].text
